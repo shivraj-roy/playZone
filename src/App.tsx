3bed299dd658decbcdbf3f9c7f117a1fbf01c5e5
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-13 20:49:52 +0530

    ? add About component with GSAP animations and content...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import About from "./components/About";
 import Hero from "./components/Hero";
 
 function App() {
    return (
       <main className="relative min-h-screen overflow-hidden w-screen">
          <Hero />
-         <section className="w-full h-screen bg-slate-400"></section>
+         <About />
       </main>
    );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,63 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+   useGSAP(() => {
+      const clipAnimation = gsap.timeline({
+         scrollTrigger: {
+            trigger: "#clip",
+            start: "center center",
+            end: "+=800 center",
+            scrub: 1,
+            pin: true,
+            pinSpacing: true,
+         },
+      });
+
+      clipAnimation.to(".mask-clip-path", {
+         width: "100vw",
+         height: "100vh",
+         borderRadius: "0%",
+      });
+   });
+
+   return (
+      <div id="about" className="min-h-screen w-screen">
+         <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+            <h2 className="font-general text-sm uppercase md:text-[10px]">
+               Welcome to PlayZone
+            </h2>
+            <div className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+               Disc<b>o</b>ver the <br />
+               <b>g</b>reatest <br />
+               <b>e</b>ntertainment
+            </div>
+            <div className="about-subtext">
+               <p>
+                  PlayZone is a platform that brings together gamers from
+                  different backgrounds and skill levels to connect, compete,
+                  and have fun.
+               </p>
+               <p>
+                  Our mission is to create a vibrant gaming community where
+                  everyone can find their place, whether you're a casual player
+                  or a hardcore gamer.
+               </p>
+            </div>
+         </div>
+         <div className="h-dvh w-screen" id="clip">
+            <div className="mask-clip-path about-image">
+               <img
+                  src="img/about.webp"
+                  alt="background"
+                  className="absolute left-0 top-0 size-full object-cover"
+               />
+            </div>
+         </div>
+      </div>
+   );
+};
+export default About;
