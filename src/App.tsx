73ac40578b3b5da29d4f8c89d8f46b148c35fa45
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-13 18:31:02 +0530

    ∵ add GSAP animations and Loader component to Hero section...

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
    return (
       <main className="relative min-h-screen overflow-hidden w-screen">
          <Hero />
+         <section className="w-full h-screen bg-slate-400"></section>
       </main>
    );
 }
---
+++ src/ui/Loader.tsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+   return (
+      <div className="three-body">
+         <div className="three-body__dot" />
+         <div className="three-body__dot" />
+         <div className="three-body__dot" />
+      </div>
+   );
+};
+export default Loader;
