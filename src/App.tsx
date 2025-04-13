946f770656a701b1d5e91cb8882d1fac9529ebfe
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-14 01:15:14 +0530

    ▬ feat: add Navbar component with audio toggle functionality and navigation links...

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
    return (
       <main className="relative min-h-screen overflow-hidden w-screen">
+         <Navbar />
          <Hero />
          <About />
       </main>
