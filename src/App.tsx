21790faf3e244ee65da1a4e62d5884e0fb5f4e0d
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-15 00:48:23 +0530

    feat: add Contact and Footer components with responsive design and social links...

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Features from "./components/Features";
 import Story from "./components/Story";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
    return (
@@ -12,6 +14,8 @@ function App() {
          <About />
          <Features />
          <Story />
+         <Contact />
+         <Footer />
       </main>
    );
 }
