bc533c276426798e179fa389aeacd273ec9a486f
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-15 01:37:33 +0530

    ⚡︎ feat: add vercel speed-insights to enhance performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { inject } from "@vercel/analytics";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import "./index.css";
 import App from "./App.tsx";
 
+injectSpeedInsights();
 inject();
 
 createRoot(document.getElementById("root")!).render(
