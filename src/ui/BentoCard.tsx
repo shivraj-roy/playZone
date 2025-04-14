44f5f8b7079f7125735d289779523015583e13f3
Shivraj Roy <shivrajroy78@gmail.com>
2025-04-15 01:42:43 +0530

    ⚒︎ refactor: remove video playback controls from BentoCard component...

--- src/ui/BentoCard.tsx
+++ src/ui/BentoCard.tsx
@@ -1,33 +1,13 @@
-import { useRef } from "react";
 import { BentoCardProps } from "../type";
 
 const BentoCard = ({ title, src, description }: BentoCardProps) => {
-   const videoRef = useRef<HTMLVideoElement>(null);
-
-   const handleMouseEnter = () => {
-      if (videoRef.current) {
-         videoRef.current.play();
-      }
-   };
-
-   const handleMouseLeave = () => {
-      if (videoRef.current) {
-         videoRef.current.pause();
-         videoRef.current.currentTime = 0; // Reset video to the beginning
-      }
-   };
-
    return (
-      <div
-         className="relative size-full"
-         onMouseEnter={handleMouseEnter}
-         onMouseLeave={handleMouseLeave}
-      >
+      <div className="relative size-full">
          <video
-            ref={videoRef}
             src={src}
             loop
             muted
+            autoPlay
             className="absolute left-0 top-0 size-full object-cover object-center"
          />
          <div className="relative z-10 flex size-full flex-col justify-start p-5 text-blue-50">
